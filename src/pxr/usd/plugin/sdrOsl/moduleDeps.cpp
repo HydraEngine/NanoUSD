0a11feb33b83d3ec6c9cfd88627f1a5479dacb72
yangfengzzz <yangfengzzz@hotmail.com>
2024-11-11 16:13:59 +0800

    save

--- src/pxr/usd/plugin/sdrOsl/moduleDeps.cpp
+++ src/pxr/usd/plugin/sdrOsl/moduleDeps.cpp
@@ -17,9 +17,19 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 TF_REGISTRY_FUNCTION(TfScriptModuleLoader) {
     // List of direct dependencies for this library.
-    const std::vector<TfToken> reqs = {TfToken("ar"),  TfToken("arch"), TfToken("gf"), TfToken("ndr"),
-                                       TfToken("sdr"), TfToken("tf"),   TfToken("vt")};
-    TfScriptModuleLoader::GetInstance().RegisterLibrary(TfToken("sdrOsl"), TfToken("pxr.SdrOsl"), reqs);
+    const std::vector<TfToken> reqs = {
+        TfToken("ar"),
+        TfToken("arch"),
+        TfToken("gf"),
+        TfToken("ndr"),
+        TfToken("sdr"),
+        TfToken("tf"),
+        TfToken("vt")
+    };
+    TfScriptModuleLoader::GetInstance().
+        RegisterLibrary(TfToken("sdrOsl"), TfToken("pxr.SdrOsl"), reqs);
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE
+
+
